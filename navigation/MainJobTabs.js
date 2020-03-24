9a1d7d767aeede8c7944be7bb5e88a3e13244304
Andre <andre2656@gmail.com>
2020-03-24 14:57:22 -0600

    Cleaned up and Noted MainJobTabs

--- navigation/MainJobTabs.js
+++ navigation/MainJobTabs.js
@@ -4,30 +4,8 @@ import Main from '../screens/MainJobScreen';
 import Measure from '../screens/MeasureScreen'
 import Install from '../screens/InstallScreen';
 import Issue from '../screens/IssueScreen'
-// class Screen extends React.Component {
-//     static navigationOptions = ({ navigation }) => ({
-//         tabBarLabel: navigation.getParam('title'),
-//         tabBarIcon: ({ tintColor }) => (
-//             <Feather size={25} name={navigation.getParam('icon')} color={tintColor} />
-//         ),
-//     });
 
-//     render() {
-//         return (
-//             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//                 <Text>{JSON.stringify(this.props.navigation.state.params)}</Text>
-//                 <Button
-//                     title="Go back"
-//                     onPress={() => this.props.navigation.goBack(null)}
-//                 />
-//             </View>
-//         );
-//     }
-// }
-
-
-//Needs styling
-
+//Navigation tabs for while a Job has been selected
 export const createSimpleTabs = (options = {}) => {
     return createBottomTabNavigator(
         {
