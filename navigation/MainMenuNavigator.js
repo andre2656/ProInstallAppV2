a569183659f8747832ac1fd09c8fe8ade3344f07
Andre <andre2656@gmail.com>
2020-03-24 15:02:00 -0600

    Added functions and style to MainMenuNav Needs Note

---
+++ navigation/MainMenuNavigator.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+import MainMenuIcon from '../components/MainMenuIcon';
+import HomeScreen from '../screens/HomeScreen';
+import JobSearchScreen from '../screens/JobSearchScreen';
+import DashboardScreen from '../screens/DashboardScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+import LogoutScreen from '../screens/LogoutScreen';
+import LoginScreen from '../screens/LoginScreen';
+const HomeStack = createStackNavigator({
+    Home: HomeScreen,
+});
+HomeStack.navigationOptions = {
+    tabBarLabel: 'Home',
+    tabBarIcon: ({ focused }) => (
+        <MainMenuIcon
+            focused={focused}
+            name={Platform.OS === 'ios' ? 'home' : 'home'}
+        />
+
+    )
+};
+const JobSearchStack = createStackNavigator({
+    JobSearch: JobSearchScreen,
+});
+JobSearchStack.navigationOptions = {
+    tabBarLabel: 'JobSearch',
+    tabBarIcon: ({ focused }) => (
+        <MainMenuIcon
+            focused={focused}
+            name={Platform.OS === 'ios' ? 'search' : 'search'}
+            navigationOptions
+        />
+    )
+};
+const DashboardStack = createStackNavigator({
+    Dashboard: DashboardScreen,
+});
+DashboardStack.navigationOptions = {
+    tabBarLabel: 'Dashboard',
+    tabBarIcon: ({ focused }) => (
+        <MainMenuIcon
+            focused={focused}
+            name={Platform.OS === 'ios' ? 'user' : 'user'}
+        />
+    )
+};
+const SettingsStack = createStackNavigator({
+    Settings: SettingsScreen,
+});
+SettingsStack.navigationOptions = {
+    tabBarLabel: 'Settings',
+    tabBarIcon: ({ focused }) => (
+        <MainMenuIcon
+            focused={focused}
+            name={Platform.OS === 'ios' ? 'cogs' : 'cogs'}
+        />
+    ),
+};
+const LogoutStack = createStackNavigator({
+    Logout: LogoutScreen,
+});
+LogoutStack.navigationOptions = {
+    tabBarLabel: 'Logout',
+    tabBarIcon: ({ focused }) => (
+        <MainMenuIcon
+            focused={focused}
+            name={Platform.OS === 'ios' ? 'sign-out' : 'sign-out'}
+        />
+    )
+};
+const LoginStack = createStackNavigator({
+    Login: LoginScreen,
+});
+LoginStack.navigationOptions = {
+    tabBarLabel: 'Login',
+    tabBarIcon: ({ focused }) => (
+        <MainMenuIcon
+            focused={focused}
+            name={Platform.OS === 'ios' ? 'sign-in' : 'sign-in'}
+        />
+    )
+};
+export default createBottomTabNavigator({
+    Home: HomeStack,
+    JobSearch: JobSearchStack,
+    Dashboard: DashboardStack,
+    Settings: SettingsStack,
+    Logout: LogoutStack,
+});
