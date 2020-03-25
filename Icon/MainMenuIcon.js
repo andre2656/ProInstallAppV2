a68aa8e4d59aebd601a6619bb7bdfb049f8a869f
Andre <andre2656@gmail.com>
2020-03-25 15:54:14 -0600

    added Icon Code and Noted

---
+++ Icon/MainMenuIcon.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Icon } from 'react-native-elements'
+import Colors from '../constants/Colors';
+
+//Icon for TabBar
+export default class TabBarIcon extends React.Component {
+    render() {
+        return (
+            <Icon
+                name={this.props.name}
+                type='font-awesome'
+                iconStyle={{ height: 100, width: '100%', marginBottom: -50, paddingLeft: '44%', paddingRight: '44%', paddingTop: 10, paddingBottom: '50%', color: 'black' }}
+                color={this.props.focused ? Colors.tabIconSelected : Colors.tabIconDefault}
+            />
+        );
+    }
+}
